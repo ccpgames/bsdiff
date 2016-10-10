dcd47aa8e4a933bf942d5ee6e50b054a1cf667c7
Snorri Sturluson <snorri.sturluson@ccpgames.com>
2016-10-10 16:23:10 +0000

    Simplifying offtout

--- bsdiff.c
+++ bsdiff.c
@@ -169,20 +169,7 @@ static int64_t search(const int64_t *I,const uint8_t *old,int64_t oldsize,
 
 static void offtout(int64_t x,uint8_t *buf)
 {
-	int64_t y;
-
-	if(x<0) y=-x; else y=x;
-
-	buf[0]=y%256;y-=buf[0];
-	y=y/256;buf[1]=y%256;y-=buf[1];
-	y=y/256;buf[2]=y%256;y-=buf[2];
-	y=y/256;buf[3]=y%256;y-=buf[3];
-	y=y/256;buf[4]=y%256;y-=buf[4];
-	y=y/256;buf[5]=y%256;y-=buf[5];
-	y=y/256;buf[6]=y%256;y-=buf[6];
-	y=y/256;buf[7]=y%256;
-
-	if(x<0) buf[7]|=0x80;
+	memcpy(buf, &x, 8);
 }
 
 static int64_t writedata(struct bsdiff_stream* stream, const void* buffer, int64_t length)
